8caf6be5d044d0c290c40aa5fdb1a2b985cc35d2
anna09 <kimgw0918@gmail.com>
2016-07-16 17:19:24 +0900

    problem3 소인수구하기 문제

---
+++ Problem3.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*
+        int flag=0;
+
+	    for(int i=3; i<=13195; i++){
+            for(int j=2; j<i; j++){
+                if(i%j==0){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag != 1) {
+                System.out.println(i);
+            }
+            flag = 0;
+        }
+
+          long index=50;
+
+        for(long i=1L; i<=index; i++){
+            if(index%i==0){
+                index=index/i;
+
+                System.out.println(i+","+index);
+            }
+            }
+        */
+
+
+        long index=50;
+
+        for(long i=1l; i<=index; i++){
+            if(index%i==0){
+                System.out.println(i);
+
+
+            }
+
+        }
+
+    }
+
+
+}
