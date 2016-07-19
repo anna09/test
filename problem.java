d153b876aefc74cdb841e879ae7a6cb72bedca20
anna09 <kimgw0918@gmail.com>
2016-07-19 15:25:58 +0900

    [#11]

---
+++ problem.java
@@ -0,0 +1,30 @@
+c class Problem3 {
+
+	public static void main(String[] args) {
+
+		//약수구하는 방법
+		long index=600851475143L;
+		/*
+		   for(long i=1; i<=index; i++){
+
+		   if(index%i==0){
+		   System.out.println(i);
+
+		   }
+		   }*/
+
+		//소인수 구하기
+
+		for(long i=1L; i<=index; i++){
+			if(index%i==0){
+				index /= i;
+
+				System.out.println(i);
+			}
+
+		}
+	}
+
+}
+
+
